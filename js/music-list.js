5e86129130441854b98eaed2894d7b9f96bcdf82
matin fallah <matinfallah81@gmail.com>
2023-11-06 00:39:06 +0330

    files added

---
+++ js/music-list.js
@@ -0,0 +1,57 @@
+let allMusic = [
+    {
+      name: "attention",
+      artist: "charlie puth",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "me gusta",
+      artist: "dft",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "ratrograde",
+      artist: "khalid",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "besmellah",
+      artist: "hiphopologist",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "am i dreaming",
+      artist: "metronoomin",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "ounana",
+      artist: "bianca costa",
+      img: "music-6",
+      src: "music-6"
+    },
+    {
+      name: "city of stars",
+      artist: "ryan gosling-emma estone",
+      img: "music-7",
+      src: "music-7"
+    },
+    {
+      name: "befour",
+      artist: "zayn",
+      img: "music-8",
+      src: "music-8"
+    },
+    {
+      name: "entertainer",
+      artist: "zayn",
+      img: "music-9",
+      src: "music-9"
+    },
+    
+  ];
